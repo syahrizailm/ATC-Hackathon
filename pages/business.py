4615f46b9248a076c4070e61aa5aa4032e96b8ea
syahrizailm <syahrizacio@gmail.com>
2022-07-10 00:44:57 +0900

    send invitation FE

--- pages/business.py
+++ pages/business.py
@@ -45,7 +45,8 @@ layout = html.Div([
                 style = {
                     "width": "80%",
                     "overflow": "scroll",
-                    "margin": "60px auto",
+                    "margin": "80px auto",
+                    "marginBottom": "0px",
                     "display": "block"
                 }
             ),
@@ -58,6 +59,28 @@ layout = html.Div([
         fullscreen = True,
         style = {"opacity": "60%"}
     ),
+    html.Div(
+        id = "button-container",
+        style = {"display": "none"},   
+        children = [
+            html.Button(
+                id = 'send-connection',
+                n_clicks = 0, 
+                children = 'Send Job Invitation',
+                style = {
+                    "margin": "10px auto",
+                    "borderColor": "#33AAFF",
+                    "color": "#33AAFF",
+                    "fontSize": "16px",
+                    "width": "80%"
+                },
+            )
+        ] 
+    ),
+    dcc.ConfirmDialog(
+        id='connection-result',
+        message='Connection sent!',
+    ),
 ])
 
 @app.callback(
@@ -65,6 +88,7 @@ layout = html.Div([
     Output('result-table', 'columns'),
     Output('get-failed', 'displayed'),
     Output('get-failed', 'message'),
+    Output('button-container', 'style'),
     Input("find-talent-button", "n_clicks"),
     State("set_skills", "value"),
     State("set_programming", "value"),
@@ -73,7 +97,7 @@ layout = html.Div([
 def get_profile(n_clicks, skills, programming, framework):
     # Don't run if submit button is never clicked
     if (n_clicks == 0):
-        return [], [], False, "Server Error"
+        return [], [], False, "Server Error", {"display": "none"}
 
     # Changing result to fit API
     condition_query = []
@@ -97,7 +121,7 @@ def get_profile(n_clicks, skills, programming, framework):
             )
     # If no input
     if not condition_query:
-        return [], [], True, "Please input condition first"
+        return [], [], True, "Please input condition first", {"display": "none"}
 
     # Get from DB
     matched = get_matching(condition_query)
@@ -107,6 +131,28 @@ def get_profile(n_clicks, skills, programming, framework):
         profile_df = profile_df.drop(columns="_id")
         data = profile_df.to_dict("records")
         columns = [{"name": i.title(), "id": i} for i in profile_df.columns]
-        return data, columns, False, "Server Error"
+
+        # Button style
+        style = {
+            "margin": "10px auto",
+            "width": "40%",
+            "display": "block"
+        }
+
+        return data, columns, False, "Server Error", style
     else:
-        return [], [], True, "Server Error"
+        return [], [], True, "Server Error", {"display": "none"}
+
+# Send invitation
+@app.callback(
+    Output('connection-result', 'displayed'),
+    Output('connection-result', 'message'),
+    Input('send-connection', 'n_clicks'),
+    State('result-table', 'data'),
+)
+def get_profile(n_clicks, data):
+    if (data == []):
+        return False, ""
+    
+    # Show that invitation is success
+    return True, f"Job Invitation to {len(data)} talent is sent!"
