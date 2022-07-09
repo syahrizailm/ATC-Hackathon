f6513de88a6488d4e3196ef8044a0b38c1165b4d
syahrizailm <syahrizacio@gmail.com>
2022-07-09 20:30:04 +0900

    portfolio FE

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from dash import Dash, dcc, html, Input, Output
+import pandas as pd
 import os
+
+from layout.candidate_info import detailed_info
+from layout.portfolio import input_field
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -8,8 +12,10 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+profiles = pd.read_csv("profile.csv")
+
 app.layout = html.Div([
-    html.H2('Hello ATC-Hackathon'),
+    input_field
 ])
 
 if __name__ == '__main__':
---
+++ data/portfolio.py
@@ -0,0 +1,25 @@
+skills = [
+    "Data Science",
+    "Front-End Development",
+    "Back-End Development",
+    "Full Stack Development",
+    "Data Engineering",
+    "Data Analyst", 
+    "Security Engineering",  
+    "DevOps",    
+]
+
+programming_language = [
+    "Python",
+    "JavaScript",
+    "R",
+    "C++",
+]
+
+framework = [ 
+    "SkLearn",
+    "TensorFlow",
+    "Django",
+    "Next.Js",
+    "Flask"
+]
